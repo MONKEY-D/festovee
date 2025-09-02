0ea7ecd4edda9b7ca9d6f22ecd412030391f5e26
Kartik Verma <kartikverma90363@gmail.com>
2025-09-02 14:46:45 +0530

    added a gsap 3d interactive component

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Shapes from "../components/Shapes";
+
+const SignUp = () => {
+  return (
+    <div>
+      <Shapes />
+    </div>
+  );
+};
+
+export default SignUp;
