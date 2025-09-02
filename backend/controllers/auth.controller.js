e863f2a8e69f9597754bd60e126800fcf64d0f32
Kartik Verma <kartikverma90363@gmail.com>
2025-09-02 10:44:43 +0530

    Sign up controller completed

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,47 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { validatePassword } from "../utils/validators.js";
+import genToken from "../utils/token.js";
+
+export const signUp = async (req, res) => {
+  try {
+    const { fullName, email, password, mobile, role } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User Already exist." });
+    }
+    if (!validatePassword(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be atleast 6 characters long, including one uppercase, one lowercase, one number and one special character.",
+      });
+    }
+    if (mobile.length < 10) {
+      return res
+        .status(400)
+        .json({ message: "Mobile number must be 10 characters" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({
+      fullName,
+      email,
+      role,
+      mobile,
+      password: hashedPassword,
+    });
+
+    const token = await genToken(user._id)
+    res.cookie("token",token,{
+        secure: false,
+        sameSite: "strict",
+        maxAge: 7*24*60*60*1000,
+        httpOnly: true
+    })
+
+    return res.status(201).json(user)
+
+
+  } catch (error) {
+    return res.status(500).json(`sign up error ${error}`)
+  }
+};
---
+++ backend/utils/validators.js
@@ -0,0 +1,6 @@
+export const validatePassword = (password) => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+  return passwordRegex.test(password);
+};
