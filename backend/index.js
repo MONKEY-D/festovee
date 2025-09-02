301461365a7fe76eeb0a4cc87dd4d18000c75292
Kartik Verma <kartikverma90363@gmail.com>
2025-09-02 11:10:48 +0530

    implemented cors and added auth routes for login, signup and logout

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
+dotenv.config();
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-dotenv.config();
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
-    connectDb
+  connectDb;
   console.log(`server started at ${port}`);
 });
