3555ba6d9978ba2530fbf49a142d56614fbf9206
Kartik Verma <kartikverma90363@gmail.com>
2025-09-04 15:37:30 +0530

    integrated multer and cloudinary

---
+++ backend/models/item.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    shop: {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: "shop",
+    },
+    category: {
+      type: String,
+      enum: [
+        "Handlooms & Textiles",
+        "Apparel & Fashion",
+        "Home & Living",
+        "Electronics & Appliances",
+        "Stationery & Office Supplies",
+        "Health & Wellness",
+        "Food & Beverages",
+        "Bamboo & Sustainable Products",
+        "Industrial & Raw Materials",
+        "Automobile & Spare Parts",
+        "Logistics & Packaging",
+        "Export-Exclusive Section",
+        "others",
+      ],
+      require: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+export default Item;
